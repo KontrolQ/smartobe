27886016bbbe67b1f005c42b0ac73ff1f3ccbab6
Jagan Mohan Reddy Danda <jmreddy2106@lbrce.ac.in>
2022-02-06 15:14:39 -0500

    removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import styles from "../styles/Home.module.css";
 const Home: NextPage = () => {
   return (
     <React.Fragment>
-      <div className="container">Hello world of Smartobe</div>
+      <div className="container">Hello world !</div>
     </React.Fragment>
   );
 };
