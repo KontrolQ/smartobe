bcf1870d332be70b92ecabf1413fb3e72c993e6f
rohit_singh_11 <rohit.singh@ankercloud.com>
2022-02-07 00:46:31 +0530

    added initial project

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <React.Fragment>
+      <div>Hello world of Smartobe</div>
+    </React.Fragment>
+  );
+};
+
+export default Home;
